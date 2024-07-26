6abd4224d9abf6313cc5dc68d3dc6e2c4a26bfd3
David <morabongs@naver.com>
2024-07-26 21:57:20 +0900

    Completing React Modal Tutorial.

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,7 +1,73 @@
+import { useState } from "react";
+import { GridColDef } from "@mui/x-data-grid";
+import Add from "../../components/add/Add";
+import DataTable from "../../components/dataTable/DataTable";
+import { products } from "../../data";
+
 import "./products.scss";
 
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "img",
+    headerName: "Image",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "title",
+    type: "string",
+    headerName: "Title",
+    width: 250,
+  },
+  {
+    field: "color",
+    type: "string",
+    headerName: "Color",
+    width: 150,
+  },
+  {
+    field: "price",
+    type: "string",
+    headerName: "Price",
+    width: 200,
+  },
+  {
+    field: "producer",
+    headerName: "Producer",
+    type: "string",
+    width: 200,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "inStock",
+    headerName: "In Stock",
+    width: 150,
+    type: "boolean",
+  },
+];
+
 const Products = () => {
-  return <div className="products">Products</div>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="products">
+      <div className="info">
+        <h1>Products</h1>
+        <button onClick={() => setOpen(true)}>Add New Products</button>
+      </div>
+      {/* slug: URL이나 데이터베이스 등의 항목을 식별하는 데 사용되는 짧고 간결한 텍스트 문자열을 의미합니다. */}
+      <DataTable slug="products" columns={columns} rows={products} />
+      {open && <Add slug="product" columns={columns} setOpen={setOpen} />}
+    </div>
+  );
 };
 
 export default Products;
