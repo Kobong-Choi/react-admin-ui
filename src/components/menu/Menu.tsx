876e1a94925776bfce8c94b0f327b0ef7a453b96
David <morabongs@naver.com>
2024-08-15 15:47:07 +0900

    Completing React Admin UI.

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -11,7 +11,7 @@ const Menu = () => {
           {item.listItems.map((listItem) => (
             <Link to={listItem.url} className="listItem" key={listItem.id}>
               <img src={listItem.icon} alt="" />
-              <span className="listItemTitlte">{listItem.title}</span>
+              <span className="listItemTitle">{listItem.title}</span>
             </Link>
           ))}
         </div>
---
+++ src/components/single/Single.tsx
@@ -0,0 +1,95 @@
+import {
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import "./single.scss";
+
+type Props = {
+  id: number;
+  img?: string;
+  title: string;
+  info: object;
+  chart?: {
+    dataKeys: { name: string; color: string }[];
+    data: object[];
+  };
+  activities?: { time: string; text: string }[];
+};
+
+const Single = (props: Props) => {
+  return (
+    <div className="single">
+      <div className="view">
+        <div className="info">
+          <div className="topInfo">
+            {props.img && <img src={props.img} alt="" />}
+            <h1>{props.title}</h1>
+            <button>Update</button>
+          </div>
+          <div className="details">
+            {/* Object.entries()는 주어진 객체의 열거 가능한 속성 [key, value] 쌍의 배열을 반환합니다. */}
+            {Object.entries(props.info).map((item) => (
+              // React에서 각 자식 요소에 고유한 key를 제공하기 위한 것으로, 이는 리스트 렌더링 시 성능 최적화 및 안정성을 위해 필수적입니다.
+              <div className="item" key={item[0]}>
+                <span className="itemTitle">{item[0]}</span>
+                <span className="itemValue">{item[1]}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+        <hr />
+        <div className="chart">
+          {props.chart && (
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart
+                width={500}
+                height={300}
+                data={props.chart.data}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                {props.chart.dataKeys.map((dataKey) => (
+                  <Line
+                    type="monotone"
+                    dataKey={dataKey.name}
+                    stroke={dataKey.color}
+                  />
+                ))}
+              </LineChart>
+            </ResponsiveContainer>
+          )}
+        </div>
+      </div>
+      <div className="activities">
+        <h2>Latest Activities</h2>
+        {props.activities && (
+          <ul>
+            {props.activities.map((activity) => (
+              <li>
+                <div>
+                  <p>{activity.text}</p>
+                  <time>{activity.time}</time>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Single;
